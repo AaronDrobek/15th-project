fd1da5225fcea2e2fcc337d4130695767debe7b6
Aaron Drobek <adrobek09@gmail.com>
2017-08-15 17:29:29 -0400

    first page content ready for syling

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const data = require("data.js")
+const data = require("./data.js");
 const express = require('express');
+const mustacheExpress = require('mustache-express');
+const path = require("path");
 const app = express();
 
-app.use("/public", express.static('public'));
+// app.use('/project', express.static(path.join(__dirname, '/project')))
+
+app.use("/public", express.static("public"));
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+
+
+app.get('/', function (req, res) {
+
+    res.render('index', data);
+})
+
+// app.get('/listing/:id', function(req, res){
+//   req.params.id
+//
+// })
+
+
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+})
